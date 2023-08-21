934d98c9b8037bbc19119d4be256447e8bdfc397
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2023-08-21 16:55:04 +0500

    Added products to homepage

---
+++ src/CarouselProduct.tsx
@@ -0,0 +1,23 @@
+
+import "./CarouselProduct.css";
+interface ProdProps{
+    logolink:string,
+    productlink: string,
+    productDesc: string,
+}
+function CarouselProduct({logolink, productlink, productDesc}: ProdProps){
+    return(
+        <>
+        <div id="containerDiv">
+            <div id="logoTextDiv">
+            <img src={logolink} id="logo"></img>
+            <p id="productDesc">{productDesc}</p>
+            </div>
+            <img src={productlink} id="productImg"></img>
+           
+        </div>
+        </>
+    );
+
+}
+export default CarouselProduct;
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,46 +1,83 @@
-import { useState, useEffect } from "react";
+// import { useEffect } from "react";
 import NavBar from "./NavBar";
-import bg1 from "./assets/background1.jpg";
-import bg2 from "./assets/background2.jpg";
+import { Carousel } from "react-responsive-carousel";
+import parts from "./assets/parts.png";
+import biotti_logo from "./assets/biotti_logo.png";
+import asteks_logo from "./assets/asteks_logo.webp";
+import tike_logo from "./assets/tike_logo.png";
+import perfect_logo from "./assets/perfect_logo.png";
+import perfect_prod from "./assets/perfect_prods.jpg";
+import tike_prod from "./assets/twist_machines.png";
+import asteks_prod from "./assets/asteks_products.jpg";
+import metallic_card from "./assets/metellic_card_clothing.jpg";
 import "./HomePage.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import CarouselProduct from "./CarouselProduct";
+
 function HomePage() {
-  const [currentImage, setCurrentImage] = useState(1);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage === 1 ? 2 : 1));
-    }, 7000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <>
       <div id="navBarDiv">
         <NavBar />
       </div>
-      <div>
-        <img
-          src={bg1}
-          id={currentImage === 1 ? "active" : "background1"}
-          alt="Background"
-        />
-        <img
-          src={bg2}
-          id={currentImage === 2 ? "active" : "background2"}
-          alt="Background"
-        />
-        <p id="introTextH1">Anchored with Excellence</p>
-        <p id="introTextH2">Textile Engineering</p>
-        <p id="introTextPara">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed
-          viverra ipsum nunc aliquet. Ultricies mi quis hendrerit dolor magna
-          eget. Scelerisque eleifend donec pretium vulputate sapien nec.
-          Faucibus scelerisque eleifend donec pretium vulputate sapien nec
-          sagittis.
-        </p>
+      <div id="introDiv">
+        <div id="introTextDiv">
+          <p id="introHeading">WHO WE ARE</p>
+          <p id="introText">
+            Welcome to your one-stop destination for premium foreign textile
+            products. From cutting-edge machinery and precision bobbins to
+            essential spare parts, we connect you to top global companies. Our
+            expertise goes beyond sales – we offer tailored advice to keep you
+            ahead in the industry. Explore excellence in textiles with us today!
+          </p>
+        </div>
+        <div>
+          <img src={parts} id="partsImg" />
+        </div>
       </div>
+      <div id="bodyDiv">
+      <div id="productHeading"><p id="productHeadingText">WHO WE REPRESENT</p></div>
+      <Carousel
+        infiniteLoop
+        showArrows
+        showIndicators
+        showThumbs
+        showStatus
+        autoFocus
+        autoPlay
+        useKeyboardArrows
+        width={"100%"}
+      >
+        <div>
+          <CarouselProduct
+            logolink={biotti_logo}
+            productlink={metallic_card}
+            productDesc="Biotti has over 140 years of experience in the textile manufacturing industry and produce the most high quality Italian metallic card clothing"
+          />
+        </div>
+        <div>
+          <CarouselProduct
+            logolink={asteks_logo}
+            productlink={asteks_prod}
+            productDesc="Experience spinning perfection with Asteks' high-quality rubber spindles, meticulously crafted for precision and durability"
+          />
+        </div>
+        <div>
+          <CarouselProduct
+            logolink={tike_logo}
+            productlink={tike_prod}
+            productDesc="Elevate your textile production with Tike's Turkish spinning machines and premium spare parts"
+          />
+        </div>
+        <div>
+          <CarouselProduct
+            logolink={perfect_logo}
+            productlink={perfect_prod}
+            productDesc="Perfect Equipment presents cutting-edge rolling cover workshop machines, revolutionizing production efficiency along with advanced textile waste lines further enhance sustainability, making excellence a part of every process"
+          />
+        </div>
+      </Carousel></div>
+      <div id="footer"></div>
     </>
   );
 }
